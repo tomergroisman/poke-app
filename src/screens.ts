dcc4205ab1fde614b7c7292007f2d742e81a1a3d
Tomer Groisman <tomergroisman@gmail.com>
2021-04-04 18:59:50 +0300

    Improved screens documentation

--- src/screens.ts
+++ src/screens.ts
@@ -1,7 +1,10 @@
 import { Navigation }from 'react-native-navigation';
 
+// Register app screens
 export function registerScreens() {
+
     Navigation.registerComponent('PokeApp.Dashboard', () => require('./pokemons/screens/Dashboard').default);
     Navigation.registerComponent('PokeApp.AddPokemon', () => require('./pokemons/screens/AddPokemon').default);
     Navigation.registerComponent('PokeApp.PokemonView', () => require('./pokemons/screens/PokemonView').default);
+
 }
